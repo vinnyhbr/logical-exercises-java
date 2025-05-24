a605df4a2256666b355865e4dd06d6564733b5ed
Vinícius Rosa <viniciusrosalima14@gmail.com>
2025-05-23 22:45:02 -0300

    feat: ÁrrayEx01

---
+++ vinicius-exercises/arrayex/ArrayEx01.java
@@ -0,0 +1,10 @@
+package arrayex;
+
+public class ArrayEx01 {
+    public static void main(String[] args) {
+        int[] nums = {10,40,4,5,4,24,53,92,3239,239,239,2};
+        for(int numero : nums){
+            System.out.println(numero);
+        }
+    }
+}
